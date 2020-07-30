4433fd9417342cc1a2d8c5672aa6943dab6f0d2e
David Hughes <hughesd9@tcd.ie>
2020-07-30 19:19:05 +0100

    Added imports and variables for keys

--- twitterBot.py
+++ twitterBot.py
@@ -1,6 +1,16 @@
-#TODO
-#Set up keys
+import tweepy
+import textblob
 
+#
+#Set up keys
+#Add your keys in the empty quotation makrs
+consumer_key = ''
+consumer_secret = ''
+
+access_token = ''
+access_token_secret = ''
+
+TODO
 #Authenticate
 
 #Do sentiment analysis
