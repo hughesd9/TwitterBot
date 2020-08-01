046856a98d4f87313e3d2dba13c5da7f0cdfa01a
David Hughes <hughesd9@tcd.ie>
2020-08-01 16:08:56 +0100

    Added user input so that the program can now search for a certain number of tweets based on a keyword and a certain polarity.

--- twitterBot.py
+++ twitterBot.py
@@ -19,9 +19,29 @@ api = tweepy.API(auth)
 #Do sentiment analysis
 #Collect tweets that contain certain hash tag
 
-tweets = api.search('#cats')
+#tweets = api.search('#cats')
 
-for tweet in tweets:
-    print(tweet.text)
-    analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
+keyword = input("Please enter a keyword you would like to search for:\n")
+print(f'You entered {keyword}')
+
+numberOfTweets = input("Please enter the number of tweets you would like to search for:\n")
+print(f'You entered {numberOfTweets}')
+numberOfTweets = int(numberOfTweets)
+
+polarityNumber = input("Please enter the minimum polarity you would like a tweet to have:")
+print(f'You entered {polarityNumber}')
+polarityNumber = int(polarityNumber)
+
+for tweet in tweepy.Cursor(api.search, keyword).items(numberOfTweets):
+    try:
+        #print(tweet.text)
+        analysis = TextBlob(tweet.text)
+        #print(analysis.sentiment)
+        #print(analysis.sentiment[0])
+        polarity = analysis.sentiment[0]
+        if(polarity > polarityNumber):
+            tweet.favorite()
+            print('Favourited tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+
