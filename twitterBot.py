49478569bebc06648638d7f2d3bdabb9aaebacea
David Hughes <hughesd9@tcd.ie>
2020-08-01 15:30:45 +0100

    Fixed the textblob import, added the authtication variables and created variable to to analysis (api), then did sentiment analysis based on tweets with a given search term.

--- twitterBot.py
+++ twitterBot.py
@@ -1,7 +1,7 @@
 import tweepy
 import textblob
+from textblob import TextBlob
 
-#
 #Set up keys
 #Add your keys in the empty quotation makrs
 consumer_key = ''
@@ -10,8 +10,18 @@ consumer_secret = ''
 access_token = ''
 access_token_secret = ''
 
-TODO
-#Authenticate
+#Authenticate and create variable to do analysis
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
 
 #Do sentiment analysis
+#Collect tweets that contain certain hash tag
 
+tweets = api.search('#cats')
+
+for tweet in tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
